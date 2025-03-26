c3a7b393fc44759d19772738943a0895a0123ce7
dlckdgh0414 <ci5976547@gmail.com>
2025-03-27 00:11:20 +0900

    fix : Enemy

--- Assets/Member/Lch/01.Scripts/Enemy/Attack/Attack.cs
+++ Assets/Member/Lch/01.Scripts/Enemy/Attack/Attack.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public abstract class Attack : MonoBehaviour
 {
+    [SerializeField] protected BulletSettingSO BulletSO;
     public abstract void EnemyAttack(Transform target);
+
 }
