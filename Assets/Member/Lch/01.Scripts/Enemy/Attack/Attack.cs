6f142717850a31a1098231de80744604f126683d
Candy <dltkr0424@gmail.com>
2025-04-03 00:20:42 +0900

    fix : attack

--- Assets/Member/Lch/01.Scripts/Enemy/Attack/Attack.cs
+++ Assets/Member/Lch/01.Scripts/Enemy/Attack/Attack.cs
@@ -1,10 +1,11 @@
+using System;
 using Member.Ysc._01_Code.Combat.Bullet;
 using UnityEngine;
 
 public abstract class Attack : MonoBehaviour
 {
     [field: SerializeField] public BaseBullet bulletPrefab { get; protected set; }
-    [field: SerializeField] public Transform FirePos { get; protected set; }
+    [field: SerializeField] public Transform[] FirePos { get; protected set; }
     
     public abstract void EnemyAttack(Transform target);
 
@@ -29,8 +30,11 @@ public abstract class Attack : MonoBehaviour
 
     protected void SpawnBullet(Transform target)
     {
-       bulletPrefab = Instantiate(bulletPrefab,FirePos.position,Quaternion.identity);
-        bulletPrefab.SetDirection(target.position);
+        for (int i = 0; i < bulletPrefab.GetBulletCount; i++)
+        {
+            bulletPrefab = Instantiate(bulletPrefab,FirePos[i].position,Quaternion.identity);
+            bulletPrefab.SetDirection(target.position);
+        }
     }
 
 }
