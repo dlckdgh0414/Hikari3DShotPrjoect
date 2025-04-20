ae26de015b3a7f7bcace46e1b2938022e4889517
Vuzuruu <144026114+ASKAUTU@users.noreply.github.com>
2025-04-20 20:56:44 +0900

    a
    a

---
+++ Assets/Scripts/UI/ImageFade.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+public class ImageFade : MonoBehaviour
+{
+    RawImage img;
+  public float colorDuraction = 1f;
+    Color imgColor;
+    float timer = 0f;
+    void Start()
+    {
+        img = GetComponent<RawImage>();
+        imgColor = img.color;
+        Debug.Log(img);
+        Debug.Log(imgColor);
+        StartCoroutine(FadeInCorutine());
+    }
+
+    IEnumerator FadeInCorutine()
+    {
+        yield return new WaitForSeconds(1.5f);
+        while (timer <= colorDuraction)
+        {
+         timer += Time.deltaTime;
+         float t = timer / colorDuraction;
+         img.color = Color.Lerp(imgColor, Color.black, t);
+            yield return null;
+        }
+        imgColor = Color.black;
+    }
+}
