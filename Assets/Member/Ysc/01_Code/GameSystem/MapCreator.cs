8137dae289f93be396aca9173e5c818f50aa889e
Candy <dltkr0424@gmail.com>
2025-04-21 23:10:40 +0900

    fix : mapCreator

--- Assets/Member/Ysc/01_Code/GameSystem/MapCreator.cs
+++ Assets/Member/Ysc/01_Code/GameSystem/MapCreator.cs
@@ -5,16 +5,14 @@ namespace Member.Ysc._01_Code.GameSystem
 {
     public class MapCreator : MonoBehaviour
     {
-        [SerializeField] private GameObject map;
         [SerializeField] private Material[] matArray;
 
         private MeshFilter _meshFilter;
         private MeshRenderer[] _meshRenderers;
         private void Awake()
         {
-            _meshFilter = map.GetComponent<MeshFilter>();
-            _meshRenderers = map.GetComponentsInChildren<MeshRenderer>();
-            _meshFilter.mesh = new Mesh();
+            _meshFilter = GetComponent<MeshFilter>();
+            _meshRenderers = GetComponentsInChildren<MeshRenderer>();
         }
 
         private void Start()
@@ -28,35 +26,6 @@ namespace Member.Ysc._01_Code.GameSystem
             {
                 _meshRenderers[i].sharedMaterial = matArray[i];
             }
-
-            CombineMesh();
-        }
-
-        private void CombineMesh()
-        {
-            MeshFilter[] meshFilters = map.GetComponentsInChildren<MeshFilter>(false);
-            CombineInstance[] combine = new CombineInstance[meshFilters.Length];
-            
-            int vertexCount = 0;
-
-            for (int i = 0; i < meshFilters.Length; i++)
-            {
-                if (meshFilters[i].sharedMesh == null) continue;
-                
-                combine[i].mesh = meshFilters[i].sharedMesh;
-                combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
-                
-                meshFilters[i].gameObject.SetActive(false);
-                
-                vertexCount += meshFilters[i].sharedMesh.vertexCount;
-            }
-            _meshFilter.mesh = new Mesh();
-            if (vertexCount > 65535)
-                _meshFilter.mesh.indexFormat = IndexFormat.UInt32;
-
-            
-            _meshFilter.mesh.CombineMeshes(combine);
-            map.gameObject.SetActive(true);
         }
     }
 }
