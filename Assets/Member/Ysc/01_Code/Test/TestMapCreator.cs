254bee7353b02b8f3d8f612c87ba936c6fcb8e75
Candy <dltkr0424@gmail.com>
2025-04-22 22:58:46 +0900

    feat : MapCreator, CreateMapObject

--- Assets/Member/Ysc/01_Code/Test/TestMapCreator.cs
+++ Assets/Member/Ysc/01_Code/Test/TestMapCreator.cs
@@ -1,20 +1,25 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Rendering;
 
-namespace Member.Ysc._01_Code.GameSystem
+namespace Member.Ysc._01_Code.Test
 {
-    public class MapCreator : MonoBehaviour
+    public class TestMapCreator : MonoBehaviour
     {
         [SerializeField] private Material[] matArray;
-        
+
+        private MeshFilter _meshFilter;
         private MeshRenderer[] _meshRenderers;
-        
         private void Awake()
         {
+            _meshFilter = GetComponent<MeshFilter>();
             _meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        }
+
+        private void Start()
+        {
             CreateMap();
         }
-        
+
         private void CreateMap()
         {
             for (int i = 0; i < _meshRenderers.Length; i++)
