85548277f7880d47c1be294ca445e2defa7c1113
Candy <dltkr0424@gmail.com>
2025-04-23 23:47:10 +0900

    test : bossEffect

--- Assets/Member/Ysc/01_Code/Agent/Enemies/BT/Boss/Actions/PlayEffectToTargetAction.cs
+++ Assets/Member/Ysc/01_Code/Agent/Enemies/BT/Boss/Actions/PlayEffectToTargetAction.cs
@@ -5,23 +5,29 @@ using Action = Unity.Behavior.Action;
 using Unity.Properties;
 
 [Serializable, GeneratePropertyBag]
-[NodeDescription(name: "PlayEffectToTarget", story: "Play [Effect] To [Target] With [StartPos] [CanTargetPos]", category: "Action", id: "b798232ef3906612e303657357206f9f")]
+[NodeDescription(name: "PlayEffectToTarget", story: "Play [Effect] To [Target] With [StartPos] [CanTargetPos] On [PatternEffect]", category: "Action", id: "b798232ef3906612e303657357206f9f")]
 public partial class PlayEffectToTargetAction : Action
 {
-    [SerializeReference] public BlackboardVariable<GameObject> Effect;
+    [SerializeReference] public BlackboardVariable<EnemyAttack> Effect;
+    [SerializeReference] public BlackboardVariable<Entity> _boss;
     [SerializeReference] public BlackboardVariable<Transform> Target;
     [SerializeReference] public BlackboardVariable<Transform> StartPos;
     [SerializeReference] public BlackboardVariable<bool> CanTargetPos;
+    [SerializeReference] public BlackboardVariable<GameObject> PatternEffect;
 
+    
     protected override Status OnStart()
     {
         if (CanTargetPos.Value)
         {
-            GameObject.Instantiate(Effect.Value, Target.Value.position, Quaternion.identity);
+            PatternEffect.Value = GameObject.Instantiate(Effect.Value.gameObject, Target.Value.position, Quaternion.identity);
+            Effect.Value._entity = _boss.Value;
         }
         else
         {
-            GameObject.Instantiate(Effect.Value,StartPos.Value.position, Effect.Value.transform.rotation);
+            PatternEffect.Value = GameObject.Instantiate(Effect.Value.gameObject, StartPos.Value.position, Effect.Value.transform.rotation);
+            Effect.Value._entity = _boss.Value;
+
         }
         return Status.Success;
     }
