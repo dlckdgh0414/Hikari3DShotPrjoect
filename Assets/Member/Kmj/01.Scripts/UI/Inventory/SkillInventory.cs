0ceb0520ced25d371014a2808a6531b8629faeb3
김민재 <kbckmj2050@gmail.com>
2025-05-01 23:40:10 +0900

    fix - skillInvenFactory

--- Assets/Member/Kmj/01.Scripts/UI/Inventory/SkillInventory.cs
+++ Assets/Member/Kmj/01.Scripts/UI/Inventory/SkillInventory.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Member.Kmin._01_Script.Core.EventChannel;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -17,24 +19,48 @@ namespace Member.Kmj._01.Scripts.UI.Inventory
         [SerializeField] private SkillSO tempSO2;
         [SerializeField] private SkillSO tempSO3;
 
+        [SerializeField] private List<Transform> childTransform;
+        
+
         private Image _image;
         
-        private SkillSO _selectedSkill;
+        public SkillSO _selectedSkill { get; set; }
 
         private void Awake()
         {
             _skillInvenEvent.AddListener<SkillSelectEvent>(HandleSkillSelect);
-            
-            equipBtns.ForEach(btn => btn.onClick.AddListener(HandleSkillEquip));
             
             _inventorySO.invenSkillDictionary.Add(tempSO1, true);
             _inventorySO.invenSkillDictionary.Add(tempSO2, true);
             _inventorySO.invenSkillDictionary.Add(tempSO3, true);
         }
 
-        private void HandleSkillEquip()
+        private void Start()
         {
+            equipBtns.ForEach(btn => btn.onClick.AddListener(HandleSkillEquip));
+        }
+
+        private void HandleSkillEquip()
+        {  
+            
+            foreach (Transform child in childTransform )
+            {
+                Image img = child.GetComponentInChildren<Image>();
+                if (img != null && img.sprite == _selectedSkill.icon)
+                {
+                    return;
+                }
+            }
+            
+            
             _image = EventSystem.current.currentSelectedGameObject.GetComponent<Image>();
+
+            if (_image.sprite == _selectedSkill.icon)
+            {
+                _image.sprite = null;
+                return;
+            }
+            
             _image.sprite = _selectedSkill.icon;
         }
 
